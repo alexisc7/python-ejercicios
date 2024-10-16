4bd457ec9b71dde1bad5fe8fe89cd00f70cceb84
alexisc7 <alexislz.k9@gmail.com>
2024-10-16 07:04:03 -0300

    refactor

--- codewars/simple_pig_latin.py
+++ codewars/simple_pig_latin.py
@@ -1,6 +1,7 @@
 """
-Simple Pig Latin
+LOL
 
+Simple Pig Latin  
 Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
 
 Examples
